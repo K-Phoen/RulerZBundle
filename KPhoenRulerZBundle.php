1a1325c2472d488d5ff16ec3e895cfe5cefc39d0
Kévin Gomez <contact@kevingomez.fr>
2015-02-09 19:11:33 +0000

    Allow to register custom operators using tagged services

--- KPhoenRulerZBundle.php
+++ KPhoenRulerZBundle.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 use KPhoen\RulerZBundle\DependencyInjection\Compiler\ExecutorsPass;
+use KPhoen\RulerZBundle\DependencyInjection\Compiler\OperatorsPass;
 
 class KPhoenRulerZBundle extends Bundle
 {
@@ -14,6 +15,7 @@ class KPhoenRulerZBundle extends Bundle
         parent::build($container);
 
         $container->addCompilerPass(new ExecutorsPass());
+        $container->addCompilerPass(new OperatorsPass());
     }
 
     /**
