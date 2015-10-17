7ae9b6e99ac1e88f7be23630aef6cc37f8f99c1f
Kévin Gomez <contact@kevingomez.fr>
2015-10-17 16:45:58 +0200

    The ValidRule now accepts a list of allowed variables

--- Validator/Constraints/RuleValidator.php
+++ Validator/Constraints/RuleValidator.php
@@ -6,6 +6,7 @@ use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 use Hoa\Compiler\Exception as CompilerException;
+use Hoa\Ruler\Model as AST;
 use RulerZ\Parser\Parser;
 
 class RuleValidator extends ConstraintValidator
@@ -20,12 +21,35 @@ class RuleValidator extends ConstraintValidator
     public function validate($rule, Constraint $constraint)
     {
         try {
-            $this->parser->parse($rule);
+            $model = $this->parser->parse($rule);
         } catch (CompilerException $e) {
             $this->context
-                ->buildViolation($constraint->message)
+                ->buildViolation($constraint->invalidMessage)
                 ->setParameter('%rule%', $rule)
                 ->addViolation();
+            return;
+        }
+
+        if ($constraint->allowed_variables === null) {
+            return;
+        }
+
+        $accesses = array_map(function(AST\Bag\Context $element) {
+            $flattenedDimensions = [$element->getId()];
+            foreach ($element->getDimensions() as $dimension) {
+                $flattenedDimensions[] = $dimension[1];
+            }
+
+            return implode('.', $flattenedDimensions);
+        }, $model->getAccesses());
+
+        foreach ($accesses as $access) {
+            if (!in_array($access, $constraint->allowed_variables)) {
+                $this->context
+                    ->buildViolation($constraint->accessNotAllowedMessage)
+                    ->setParameter('%access%', $access)
+                    ->addViolation();
+            }
         }
     }
 }
--- Validator/Constraints/ValidRule.php
+++ Validator/Constraints/ValidRule.php
@@ -3,13 +3,37 @@
 namespace KPhoen\RulerZBundle\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Exception\MissingOptionsException;
 
 /**
  * @Annotation
  */
 class ValidRule extends Constraint
 {
-    public $message = 'The rule « %rule% » is invalid.';
+    public $invalidMessage          = 'The rule « %rule% » is invalid.';
+    public $accessNotAllowedMessage = 'The variable « %access% » does not exist.';
+
+    public $allowed_variables;
+
+    public function __construct($options = null)
+    {
+        $options = array_merge([
+            'allowed_variables' => null,
+        ], $options);
+
+        parent::__construct($options);
+
+        if (!array_key_exists('allowed_variables', $options)) {
+            throw new MissingOptionsException(sprintf('Option "allowed_variables" must be given for constraint %s', __CLASS__), ['allowed_variables']);
+        }
+    }
+
+    public function getRequiredOptions()
+    {
+        return [
+            'allowed_variables'
+        ];
+    }
 
     public function validatedBy()
     {
