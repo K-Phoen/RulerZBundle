88957e836c59632f4907f0021d9abf062908c6fb
Kévin Gomez <kgomez@tea-ebook.com>
2017-09-25 22:27:18 +0200

    Removed unused 'use' statements

--- Validator/Constraints/ValidRule.php
+++ Validator/Constraints/ValidRule.php
@@ -2,9 +2,6 @@
 
 namespace KPhoen\RulerZBundle\Validator\Constraints;
 
-use Symfony\Component\Validator\Constraint;
-use Symfony\Component\Validator\Exception\MissingOptionsException;
-
 use Symfony\Bridge\RulerZ\Validator\Constraints\ValidRule as BridgeValidRule;
 
 /**
